d7300b5c57ec595b8827e764660a31424bdca46e
kuro.kuro <japanesebrothers@gmail.com>
2020-09-26 23:29:50 +0900

    Modify UpdateServlet.java

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -21,8 +21,8 @@ import utils.DBUtil;
  */
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -31,46 +31,41 @@ public class UpdateServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String _token = request.getParameter("_token");
-		if(_token != null && _token.equals(request.getSession().getId())){
-		    EntityManager em = DBUtil.createEntityManager();
-		    
-		    Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
-		    
-		    String content = request.getParameter("content");
-		    t.setContent(content);
-		    
-		    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-		    t.setUpdated_at(currentTime);
-		    
-		    List<String> errors = TaskValidator.validate(t);
-		    if(errors.size() > 0){
-		        request.setAttribute("_token", request.getSession().getId());
-		        request.setAttribute("task", t);
-		        request.setAttribute("errors", errors);
-		        
-		        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/edit.jsp");
-		        rd.forward(request,response);
-		    }else{
-		        em.getTransaction().begin();
-		        em.getTransaction().commit();
-		        request.getSession().setAttribute("flush", "更新が完了しました。");
-		        em.close();
-		    }
-		    
-		    em.getTransaction().begin();
-		    em.getTransaction().commit();
-		    request.getSession().setAttribute("flush", "更新が完了しました。");
-		    em.close();
-		    
-		    request.getSession().removeAttribute("task_id");
-		    
-		    response.sendRedirect(request.getContextPath() + "/index");
-		}
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em = DBUtil.createEntityManager();
+
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setUpdated_at(currentTime);
+
+            List<String> errors = TaskValidator.validate(t);
+            if(errors.size() > 0){
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", t);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/edit.jsp");
+                rd.forward(request,response);
+            }else{
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+                em.close();
+
+                request.getSession().removeAttribute("task_id");
+
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
+        }
+    }
 
 }
